73f5f6019ee2223db92d1ec9206515696d2350db
Ricardo Muruaga <richardmuru25@gmail.com>
2023-06-03 18:22:11 -0300

    codeando

--- main.js
+++ main.js
@@ -1,27 +1,54 @@
-const clickButton = document.querySelectorAll("button")
+    const clickButton = document.querySelectorAll("button")
 
-
-
-
-const tienda = document.getElementById("tienda")
-
-
-
-const productos = [
-    {  id: 1, nombre: "Camiseta titular Hombre", precio: 22.500}, 
-    {  id: 2, nombre: "Short Hombre", precio: 10.000},  
-    {  id: 3, nombre: "Camiseta suplente Mujer", precio: 20.000},  
-    {  id: 4, nombre: "Short niño", precio: 8.000}
-    ];
-    
+    clickButton.forEach(button =>{
+        button.addEventListener ("click", addToCarrito)
+    })
     
     let carrito = []
     
-    productos.forEach((product) => {
-    let productos = document.createElement("div");
-    productos.innerHTML = `
-        <h3=${producto.nombre}">
-        <p= ${producto.precio}">
+    function addToCarrito(e){
+      const button = e.target
+      const item = button.closest('.card')  
+      const nombrePrenda = item.querySelector('.card-title').textContent;
+      const precio = item.querySelector('.card-text').textContent;
+      const imagen = item.querySelector('.card-img-top').src;
+      console.log(imagen);
+        
+      const newItem = {
+        titulo: nombrePrenda,
+        precio: precio,
+        imagen: imagen,
+        cantidad: 1,
+    
+    }
+     addItemCarrito(newItem)
+ }
+     
+    function addItemCarrito(newItem){
+        carrito.push(newItem)
+        renderCarrito()
+       
+    }
+
+    function renderCarrito (){
+        console.log(carrito)
+    }
+    
+   
+  /* const product = [
+        {  id: 1, nombre: "Camiseta titular Hombre", precio: 22.500}, 
+        {  id: 2, nombre: "Short Hombre", precio: 10.000},  
+        {  id: 3, nombre: "Camiseta suplente Mujer", precio: 20.000},  
+        {  id: 4, nombre: "Short niño", precio: 8.000}
+        ]; 
+      */
+     
+    
+   /*  productos.forEach((product) => {
+    let product = document.createElement("div");
+    product.innerHTML = `
+        <h3=${product.nombre}">
+        <p= ${product.precio}">
     `;
     
     tienda.append(productos)
@@ -29,4 +56,4 @@ const productos = [
     }
     );
 
-  carrito.push
+  carrito.push    */
