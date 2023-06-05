9bec69fe7bb77bc70067a452f9a7f5e81887e89d
Ricardo Muruaga <richardmuru25@gmail.com>
2023-06-05 16:09:32 -0300

    practicando

--- main.js
+++ main.js
@@ -14,8 +14,7 @@
       const nombrePrenda = item.querySelector('.card-title').textContent;
       const precio = item.querySelector('.card-text').textContent;
       const imagen = item.querySelector('.card-img-top').src;
-      console.log(imagen);
-        
+      
       const newItem = {
         titulo: nombrePrenda,
         precio: precio,
@@ -37,27 +36,18 @@
     }
     
     carritoCompra.addEventListener("click", () =>{
+        carrito.push({
+
+        })
    })
    
-  /* const product = [
-        {  id: 1, nombre: "Camiseta titular Hombre", precio: 22.500}, 
-        {  id: 2, nombre: "Short Hombre", precio: 10.000},  
-        {  id: 3, nombre: "Camiseta suplente Mujer", precio: 20.000},  
-        {  id: 4, nombre: "Short niño", precio: 8.000}
-        ]; 
-      */
-     
-    
-   /*  productos.forEach((product) => {
-    let product = document.createElement("div");
-    product.innerHTML = `
-        <h3=${product.nombre}">
-        <p= ${product.precio}">
-    `;
-    
-    tienda.append(productos)
+  
 
-    }
-    );
-
-  carrito.push    */
+  
+  
+  
+  
+  
+  
+  
+  
